07f4bcb589a7b1a34691ed53c58f0e29a04ecbfc
Vincent Guittot <vincent.guittot@linaro.org>
2026-01-21 17:33:17 +0100

    sched/fair: Fix pelt clock sync when entering idle
    Samuel and Alex reported regressions of the util_avg of RT rq with
    commit 17e3e88ed0b6 ("sched/fair: Fix pelt lost idle time detection").
    It happens that fair is updating and syncing the pelt clock with task one
    when pick_next_task_fair() fails to pick a task but before the prev
    scheduling class got a chance to update its pelt signals.
    
    Move update_idle_rq_clock_pelt() in set_next_task_idle() which is called
    after prev class has been called.
    
    Fixes: 17e3e88ed0b6 ("sched/fair: Fix pelt lost idle time detection")
    Closes: https://lore.kernel.org/all/CAG2KctpO6VKS6GN4QWDji0t92_gNBJ7HjjXrE+6H+RwRXt=iLg@mail.gmail.com/
    Closes: https://lore.kernel.org/all/8cf19bf0e0054dcfed70e9935029201694f1bb5a.camel@mediatek.com/
    Reported-by: Samuel Wu <wusamuel@google.com>
    Reported-by: Alex Hoh <Alex.Hoh@mediatek.com>
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Samuel Wu <wusamuel@google.com>
    Tested-by: Alex Hoh <Alex.Hoh@mediatek.com>
    Link: https://patch.msgid.link/20260121163317.505635-1-vincent.guittot@linaro.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9051,12 +9051,6 @@ idle:
 			goto again;
 	}
 
-	/*
-	 * rq is about to be idle, check if we need to update the
-	 * lost_idle_time of clock_pelt
-	 */
-	update_idle_rq_clock_pelt(rq);
-
 	return NULL;
 }
 
