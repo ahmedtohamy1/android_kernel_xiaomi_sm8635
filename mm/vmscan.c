402de808d752e98a39f3c8f97e91a0d771954e46
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2026-05-19 03:15:13 +0000

    test

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -210,7 +210,9 @@ static void set_task_reclaim_state(struct task_struct *task,
 	task->reclaim_state = rs;
 }
 
+
 static LIST_HEAD(shrinker_list);
+static DECLARE_RWSEM(shrinker_rwsem);
 static DEFINE_SPINLOCK(shrinker_lock);
 static DEFINE_RWLOCK(shrinker_rwlock);
 
